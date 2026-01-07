0236a4b2a6baa60987d1bee34e1d6a8005f62601
Shiva82804 <shivaaithagoni111@gmail.com>
2026-01-07 12:43:14 +0530

    vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ app.use("/api/messages",messageRoutes);
 if(process.env.NODE_ENV === "production" ) {
     app.use(express.static(path.join(__dirname,"../fronten/dist")))
     app.get("*",(req,res) =>{
-        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+        res.sendFile(path.join(__dirname,"frontend/dist/index.html"))
     })
 }
 app.listen(PORT,()=>console.log("The Server is listening"));
