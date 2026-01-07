22ff2b94a1bebaf38b0972a6cce522a552bb6cf4
Shiva82804 <shivaaithagoni111@gmail.com>
2026-01-07 22:32:12 +0530

    DB

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import {connectDB} from "./lib/db.js";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -18,3 +19,4 @@ if(process.env.NODE_ENV === "production" ) {
     })
 }
 app.listen(PORT,()=>console.log("The Server is listening"));
+connectDB 
