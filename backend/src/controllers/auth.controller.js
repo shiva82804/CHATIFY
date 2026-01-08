9dec7cf4ec10500a5e4c3afc94bd3e6d041e898e
Shiva82804 <shivaaithagoni111@gmail.com>
2026-01-08 22:56:06 +0530

    User model

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt.js";
 export const signup = async (req,res)=> {
     const {fullName,email,password} = req.body
     try {
@@ -7,6 +9,17 @@ export const signup = async (req,res)=> {
         if(password.length< 6){
             return res.status(400).json({message:"Password must be at least 8 characters"});
         }
+        const emailRegex = /^[^\s@]+@ [^\s@]+\. [^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format"});
+        }
+
+        const user  = await User.findOne({email});
+        if(user)
+            return res.status(400).json({ message: "Email already exists"});
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt);
     } catch (error) {
         
     }
