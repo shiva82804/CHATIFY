e31adf043ed64d4f34f9c8811a0c2d9926f7f472
Shiva82804 <shivaaithagoni111@gmail.com>
2026-01-11 13:15:59 +0530

    Login and Logout endpoint

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -53,3 +53,36 @@ export const signup = async (req,res)=> {
     }
     
 };
+
+export const login = async (req,res)=> {
+    const {email, password} = req.body
+    try {
+        const user =  await User.findOne({email});
+        if(!user) return res.status(400).json({message: "Invalid Credentials"});
+        //Credentials missing should not be exposed to User
+        
+        const isPasswordCorrect = await bcrypt.compare(password,user.password)
+        if(!isPasswordCorrect) return res.status(400).json({message: "Invalid Credentials"});
+
+        generateToken(user._id,res)
+        res.status(200).json(
+            {
+                _id : user._id,
+                fullName: user.fullName,
+                email: user.email,
+                profilePic:  user.profilePic,
+            }
+        );
+    } catch (error) {
+        console.log("Error in login account");
+        res.status(500).json({ 
+        message: "Internal server error", 
+        error: error.message, // This tells you exactly what happened
+    });
+    }
+}
+
+export const logout = async (req,res)=> {
+    res.cookie("jwt","",{maxAge: 0});
+    res.status(200).json({message:"Logout Success"});
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,13 +1,9 @@
-import { signup } from "../controllers/auth.controller.js";
+import { signup,login,logout } from "../controllers/auth.controller.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/signup",signup);
-router.get("/login",(req,res)=> {
-    res.send("login endpoint");
-})
-router.get("/logout",(req,res)=> {
-    res.send("logout endpoint");
-})
+router.post("/login",login);
+router.post("/logout",logout);
 export default router;
